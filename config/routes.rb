d0e8ba8741d32bba263d419c18a1545ae90de7c2
kuliodev <sobegogo@gmail.com>
2017-07-25 03:52:27 +0000

    add history page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
     root 'pages#home'
   
     get 'seller' => 'listings#seller'
-  
+ 
+    get 'sales' => "orders#sales"
+    get 'purchases' => "orders#purchases"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
